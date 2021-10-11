c6e1380bd5ec85346c5124268c61cec463dc2e3b
Sascha Vis <saschavis@gmail.com>
2021-10-11 10:58:24 +0200

    Used HomeAssistant library

--- WeatherStation/WeatherStation.ino
+++ WeatherStation/WeatherStation.ino
@@ -1,241 +1,162 @@
-/*
-   Author: Sascha Vis
-   Student number: 0962873
-   Date created: 07/09/2021
-   Last modified: 07/09/2021
-   Github: https://github.com/Lenteguppie/WeatherStation-Hardware
-   Board: ESP32
-*/
 #include "secrets.h"
 
+//Include the required libraries
 #include <WiFi.h>
-#include <PubSubClient.h>
-#include <b64.h>
+#include <ArduinoHA.h>
 #include "DHT.h"
-#include <ArduinoJson.h>
 
-//Deep sleep Constants
-#define uS_TO_S_FACTOR 1000000ULL  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  60        /* Time ESP32 will go to sleep (in seconds) */
-
-//DHT11 Constants
-#define DHTPIN 14
+//Define DHT sensor pin and type
+#define DHTPIN 4
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
 
-// Define some variables we need later
-float humidity;
-float temperature;
+//Define variables
+unsigned long lastReadAt = millis();
+unsigned long lastTemperatureSend = millis();
+bool lastInputState = false;
 
-float wind_speed;
+float temperatureValue;
+float humidityValue;
+float signalstrengthValue;
+float moistureValue;
 
-int sensorPin = A0;
-int moisture = 0;
+//Initialize WiFi
+WiFiClient client;
+HADevice device;
+HAMqtt mqtt(client, device);
 
-int moisture_low = 650;
-int moisture_high = 350;
-int moisturePercentage = 0;
-
-// My own numerical system for registering devices.
-String student_number = STUDENT_NUMBER;
-String mqttName = "HomeStation " + String(student_number);
-String stateTopic = "home_station/weather/" + String(student_number) + "/state";
-
-WiFiClient wifiClient;
-PubSubClient client(wifiClient);
-
-void sendSensorData() {
-  humidity = dht.readHumidity();
-  temperature = dht.readTemperature();
-  moisture = analogRead(sensorPin);
-
-  if (isnan(humidity)) {
-    humidity = 0;
-  }
-
-  if (isnan(temperature)) {
-    temperature = 0;
-  }
-
-  wind_speed = 0;
-
-  // Map moisture sensor values to a percentage value
-  moisturePercentage = map(moisture, moisture_low, moisture_high, 0, 100);
-
-  DynamicJsonDocument doc(1024);
-  char buffer[256];
-  doc["windspeed"] = wind_speed;
-  doc["humidity"] = humidity;
-  doc["temperature"]   = temperature;
-  doc["moisture"] = moisturePercentage;
-
-  size_t n = serializeJson(doc, buffer);
-
-  bool published = client.publish(stateTopic.c_str(), buffer, n);
-
-  // Print the sensor values to Serial out (for debugging)
-  Serial.println("published: ");
-  Serial.println(published);
-  Serial.println("humidity: ");
-  Serial.println(humidity);
-  Serial.println("temperature: ");
-  Serial.println(temperature);
-  Serial.println("moisture %: ");
-  Serial.println(moisturePercentage);
-}
+//Define the sensors and/or devices
+//The string must not contain any spaces!!! Otherwise the sensor will not show up in Home Assistant
+HASensor sensorLong("Long");
+HASensor sensorLat("Lat");
+HASensor sensorTemperature("Temperature");
+HASensor sensorHumidity("Humidity");
+HASensor sensorMoisture("Moisture");
+HASensor sensorSignalstrength("Signal_strength");
 
 void setup() {
   Serial.begin(9600);
-  delay(500); //Get some time to start up the serial monitor
+  Serial.println("Starting...");
 
-  /*
-    First we configure the wake up source
-    We set our ESP32 to wake up every 5 seconds
-  */
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
-  Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) +
-                 " Seconds");
+  // Unique ID must be set!
+  byte mac[6];
+  WiFi.macAddress(mac);
+  device.setUniqueId(mac, sizeof(mac));
 
-  //Start the WiFi connection
-  WiFi.begin(WIFI_SSID, WIFI_PASS);
-
-  Serial.println("Connecting to Wi-Fi");
-
+  // Connect to wifi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
     Serial.print(".");
+    delay(500); // waiting for the connection
+  }
+  Serial.println();
+  Serial.println("Connected to the network");
+
+  // Set sensor and/or device names
+  // String conversion for incoming data from Secret.h
+  String student_id = STUDENT_ID;
+  String student_name = STUDENT_NAME;
+
+  //Add student ID number with sensor name
+  String stationNameStr = student_name + "'s Home Station";
+  String longNameStr = student_id + " Long";
+  String latNameStr = student_id + " Lat";
+  String temperatureNameStr = student_id + " Temperature";
+  String humidityNameStr = student_id + " Humidity";
+  String moistureNameStr = student_id + " Moisture";
+  String signalstrengthNameStr = student_id + " Signal Strength";
+
+  //Convert the strings to const char*
+  const char* stationName = stationNameStr.c_str();
+  const char* longName = longNameStr.c_str();
+  const char* latName = latNameStr.c_str();
+  const char* temperatureName = temperatureNameStr.c_str();
+  const char* humidityName = humidityNameStr.c_str();
+  const char* moistureName = moistureNameStr.c_str();
+  const char* signalstrengthName = signalstrengthNameStr.c_str();
+
+  //Set main device name
+  device.setName(stationName);
+  device.setSoftwareVersion(SOFTWARE_VERSION);
+  device.setManufacturer(STUDENT_NAME);
+  device.setModel(MODEL_TYPE);
+
+  sensorLong.setName(longName);
+  sensorLong.setIcon("mdi:crosshairs-gps");
+  sensorLat.setName(latName);
+  sensorLat.setIcon("mdi:crosshairs-gps");
+
+  sensorTemperature.setName(temperatureName);
+  sensorTemperature.setDeviceClass("temperature");
+  sensorTemperature.setUnitOfMeasurement("°C");
+
+  sensorHumidity.setName(humidityName);
+  sensorHumidity.setDeviceClass("humidity");
+  sensorHumidity.setUnitOfMeasurement("%");
+
+  sensorMoisture.setName(moistureName);
+  sensorMoisture.setDeviceClass("moisture");
+  sensorMoisture.setUnitOfMeasurement("%");
+
+  sensorSignalstrength.setName(signalstrengthName);
+  sensorSignalstrength.setDeviceClass("signal_strength");
+  sensorSignalstrength.setUnitOfMeasurement("dBm");
+
+  mqtt.begin(BROKER_ADDR, BROKER_USERNAME, BROKER_PASSWORD);
+
+  while (!mqtt.isConnected()) {
+    mqtt.loop();
+    Serial.print(".");
+    delay(500); // waiting for the connection
   }
 
-  WiFi.setAutoReconnect(true);
-  WiFi.persistent(true);
+  Serial.println();
+  Serial.println("Connected to MQTT broker");
 
-  Serial.println("Connected to Wi-Fi");
+  sensorLat.setValue(LAT, (uint8_t)15U);
+  sensorLong.setValue(LONG, (uint8_t)15U);
 
-  client.setServer(MQTT_BROKER, MQTT_PORT);
-  Serial.println("Connecting to MQTT");
-  
-  bool resized = client.setBufferSize(512); //resizes the buffer because the discovery payloads can be pretty big
-  Serial.println("resized buffer: " + String(resized));
-
-  while (!client.connected()) {
-    Serial.print(".");
-
-    if (client.connect(mqttName.c_str(), MQTT_USER, MQTT_PASSWORD)) {
-
-      Serial.println("Connected to MQTT");
-
-      sendMQTTTemperatureDiscoveryMsg();
-      delay(500);
-      sendMQTTHumidityDiscoveryMsg();
-      delay(500);
-      sendMQTTMoistureDiscoveryMsg();
-      delay(500);
-      sendMQTTWindSpeedDiscoveryMsg();
-      delay(500);
-
-      //Start the DHT
-      dht.begin();
-
-      Serial.println("===== Sending Data =====");
-      sendSensorData(); //Get the sensorData and send it over to the MQTT broker
-
-    } else {
-
-      Serial.println("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-
-    }
-  }
-
-  //Go into deep sleep...
-  Serial.println("Going to sleep now");
-  Serial.flush();
-  esp_deep_sleep_start();
-  Serial.println("This will never be printed");
-}
-
-void sendMQTTTemperatureDiscoveryMsg() {
-  Serial.println("Sending Temperature Discovery MSG");
-  String discoveryTopic = "homeassistant/sensor/homestation_sensor_" + String(student_number) + "/temperature/config";
-
-  DynamicJsonDocument doc(1024);
-  char buffer[256];
-
-  doc["name"] = "Homestation" + String(student_number) + " Temperature";
-  doc["stat_t"]   = stateTopic;
-  doc["unit_of_meas"] = "°C";
-  doc["dev_cla"] = "temperature";
-  doc["frc_upd"] = true;
-  doc["val_tpl"] = "{{ value_json.temperature|default(0) }}";
-
-  size_t n = serializeJson(doc, buffer);
-
-  bool sent = client.publish(discoveryTopic.c_str(), buffer, n);
-  Serial.println("Send to topic: " + String(sent));
-
-}
-
-void sendMQTTHumidityDiscoveryMsg() {
-  Serial.println("Sending Humidity Discovery MSG");
-  String discoveryTopic = "homeassistant/sensor/homestation_sensor_" + String(student_number) + "/humidity/config";
-
-  DynamicJsonDocument doc(1024);
-  char buffer[256];
-
-  doc["name"] = "Homestation " + String(student_number) + " Humidity";
-  doc["stat_t"]   = stateTopic;
-  doc["unit_of_meas"] = "%";
-  doc["dev_cla"] = "humidity";
-  doc["frc_upd"] = true;
-  doc["val_tpl"] = "{{ value_json.humidity|default(0) }}";
-
-  size_t n = serializeJson(doc, buffer);
-
-  bool sent = client.publish(discoveryTopic.c_str(), buffer, n);
-  Serial.println("Send to topic: " + String(sent));
-}
-
-void sendMQTTMoistureDiscoveryMsg() {
-  Serial.println("Sending Moisture Discovery MSG");
-  String discoveryTopic = "homeassistant/sensor/homestation_sensor_" + String(student_number) + "/moisture/config"; // The topic the sensor will post the entity configuration to.
-
-  //Initialize the JSON Document
-  DynamicJsonDocument doc(1024);
-  char buffer[256];
-
-  doc["name"] = "Homestation " + String(student_number) + " Moisture"; //The name of the sensor
-  doc["stat_t"]   = stateTopic; //The topic the sensor will send the updates to
-  doc["frc_upd"] = true; //Force update
-  doc["val_tpl"] = "{{ value_json.moisture|default(0) }}"; //Value template. This will define how HASSIO will parse the data.
-
-  size_t n = serializeJson(doc, buffer); //Convert the JSON document to a String
-  
-  bool sent = client.publish(discoveryTopic.c_str(), buffer, n); //Publish the MQTT message to the discovery topic
-  Serial.println("Send to topic: " + String(sent));
-}
-
-void sendMQTTWindSpeedDiscoveryMsg() {
-  Serial.println("Sending Wind Speed Discovery MSG");
-  String discoveryTopic = "homeassistant/sensor/homestation_sensor_" + String(student_number) + "/wind_speed/config"; // The topic the sensor will post the entity configuration to.
-
-  //Initialize the JSON Document
-  DynamicJsonDocument doc(1024);
-  char buffer[256];
-
-  doc["name"] = "Homestation " + String(student_number) + " Wind Speed"; //The name of the sensor
-  doc["stat_t"]   = stateTopic; //The topic the sensor will send the updates to
-  doc["dev_cla"] = "speed";
-  doc["unit_of_meas"] = "m/s";
-  doc["frc_upd"] = true; //Force update
-  doc["val_tpl"] = "{{ value_json.windspeed|default(0) }}"; //Value template. This will define how HASSIO will parse the data.
-
-  size_t n = serializeJson(doc, buffer); //Convert the JSON document to a String
-
-  bool sent = client.publish(discoveryTopic.c_str(), buffer, n); //Publish the MQTT message to the discovery topic
-  Serial.println("Send to topic: " + String(sent));
+  dht.begin();
 }
 
 void loop() {
-  //This is not going to be used
+  mqtt.loop();
+
+  moistureValue = 0.0;
+  humidityValue = dht.readHumidity();
+  temperatureValue = dht.readTemperature();
+  signalstrengthValue = WiFi.RSSI();
+
+  if (isnan(temperatureValue)) {
+    signalstrengthValue = 0;
+  }
+
+  if (isnan(temperatureValue)) {
+    temperatureValue = 0;
+  }
+
+  if ((millis() - lastTemperatureSend) > 10000) { // read in 30ms interval
+
+    sensorTemperature.setValue(temperatureValue);
+    Serial.print("Current temperature is: ");
+    Serial.print(temperatureValue);
+    Serial.println("°C");
+
+    sensorHumidity.setValue(humidityValue);
+    Serial.print("Current humidity is: ");
+    Serial.print(humidityValue);
+    Serial.println("%");
+
+    sensorMoisture.setValue(moistureValue);
+    Serial.print("Current moisture is: ");
+    Serial.print(moistureValue);
+    Serial.println("%");
+
+    sensorSignalstrength.setValue(signalstrengthValue);
+    Serial.print("Current signal strength is: ");
+    Serial.print(signalstrengthValue);
+    Serial.println("%");
+
+    lastTemperatureSend = millis();
+  }
 }
--- WeatherStation/secrets.h
+++ WeatherStation/secrets.h
@@ -1,12 +1,20 @@
-// Use this file to store all of the private credentials 
-// and connection details
+//Personal credentials
+#define STUDENT_NAME    "Sascha"                       //Your Name
+#define STUDENT_ID      "0962873"       //Your HR student ID number
 
-const char *WIFI_SSID = "MySSID";    // Replace MySSID with your WiFi network name
-const char *WIFI_PASS = "MyPassword";  // Replace MyPassword with your WiFi password
+//WiFi credentials
+#define WIFI_SSID       "Aquarium"                       //Tesla IoT or your local network
+#define WIFI_PASSWORD   "31Guppie23"
 
-const char *MQTT_BROKER = "Broker adress";     // Replace broker address with your broker host address
-#define MQTT_PORT 1883   // MQTT default is 1883, change this to the port your broker has
-const char *MQTT_USER = "USERNAME"; // Replace USERNAME with your MQTT username
-const char *MQTT_PASSWORD = "PASSWORD"; // Replace PASSWORD with your MQTT password
+// MQTT credentials
+#define BROKER_ADDR     "s-weather.aquariumnetwork.tech"  // Centralized server adress
+#define BROKER_USERNAME "sascha"                   // Your MQTT username
+#define BROKER_PASSWORD "LGuppie"                   // Your MQTT password
 
-#define STUDENT_NUMBER "STUDENT_NUMBER" // Replace STUDENT_NUMBER with your own student number
+// Location
+#define LAT              51.749371               //Your Home Stations location
+#define LONG             4.551949
+
+//Device model and software version
+#define SOFTWARE_VERSION "1.69.420"                          //Software version number
+#define MODEL_TYPE       "Dankvice"                //Model of your weather station
